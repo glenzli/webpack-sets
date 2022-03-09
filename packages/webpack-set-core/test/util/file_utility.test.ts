44c90d0c3adbc628e2717aaa7b981905bf8f599c
glenzli <glenzli@outlook.com>
2022-03-10 01:21:11 +0800

    test: 💍 add util test

---
+++ packages/webpack-set-core/test/util/file_utility.test.ts
@@ -0,0 +1,14 @@
+import 'jest';
+import path from 'path';
+import { FileUtility } from '../../src/util/file_utility';
+
+describe('File Utility Test', () => {
+    test('find', () => {
+        const dirs = [__dirname, path.parse(__dirname).dir];
+        const filePath = __filename;
+        const fileName = `${path.parse(filePath).name}${path.parse(filePath).ext}`;
+        expect(FileUtility.find(fileName, dirs)).toEqual(filePath);
+        expect(FileUtility.find(fileName, [dirs[1]])).toBeUndefined();
+        expect(FileUtility.find(filePath, dirs)).toBeUndefined();
+    });
+});
---
+++ packages/webpack-set-core/test/util/module_utility.test.ts
@@ -0,0 +1,39 @@
+import 'jest';
+import fs from 'fs';
+import { ModuleUtility } from '../../src/util/module_utility';
+
+describe('File Utility Test', () => {
+    test('resolvePackageJson node_modules', () => {
+        const pkgJson = ModuleUtility.resolvePackageJson(require.resolve('typescript'));
+        expect(pkgJson).not.toBeUndefined();
+        if (pkgJson) {
+            expect(fs.existsSync(pkgJson)).toBe(true);
+            expect(JSON.parse(fs.readFileSync(pkgJson, { encoding: 'utf-8' })).name).toEqual('typescript');
+        }
+    });
+
+    test('resolvePackageJson project file', () => {
+        const pkgJson = ModuleUtility.resolvePackageJson(__filename);
+        expect(pkgJson).not.toBeUndefined();
+        if (pkgJson) {
+            expect(fs.existsSync(pkgJson)).toBe(true);
+            expect(JSON.parse(fs.readFileSync(pkgJson, { encoding: 'utf-8' })).name).toEqual('@g4iz/webpack-set-core');
+        }
+    });
+
+    test('resolveTypeFile own', () => {
+        const typeFile = ModuleUtility.resolveTypeFile(ModuleUtility.resolveModuleEntry('webpack'));
+        expect(typeFile).not.toBeUndefined();
+        if (typeFile) {
+            expect(typeFile).toMatch(/webpack[\\/].*\.d\.ts$/);
+        }
+    });
+
+    test('resolveTypeFile external', () => {
+        const typeFile = ModuleUtility.resolveTypeFile(ModuleUtility.resolveModuleEntry('@babel/core'));
+        expect(typeFile).not.toBeUndefined();
+        if (typeFile) {
+            expect(typeFile).toMatch(/babel__core[\\/].*\.d\.ts$/);
+        }
+    });
+});
